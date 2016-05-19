31354d3791407c8c3399a63d1aba2612983ab8ce
mahoto <mahotox101500@gmail.com>
2016-05-20 00:24:04 +0900

    mod class

--- Initialize/CollectBrandPrice.py
+++ Initialize/CollectBrandPrice.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import csv
 import jsm
 import datetime
 
@@ -16,13 +17,27 @@ class CollectBrandPrices:
         テストモジュールのため./Initialize/brand_prices.csvを出力します。
         TODO: MonboDBに格納し、利用しやすい形に成型する
         '''
-        q = jsm.QuotesCsv()
+        q = jsm.Quotes()
         for code in self.brand_codes:
-            q.save_historical_prices(code + '.csv', code, jsm.DAILY, start_date, end_date)
-
+            print(code[0])
+            try:
+                data = q.get_historical_prices(code[0], jsm.DAILY, start_date, end_date)
+                f = open('data'+str(code[0])+'.csv', 'w')
+                writer = csv.writer(f, lineterminator="\n")
+                writer.writerow(data)
+                #print(data)
+            except:
+                pass
+            
+            
+          
 
 if __name__ == '__main__':
-    f = open('Initialize/brand_codes.csv')
-    codes = f.readlines()
+    f = open('brand_codes.csv', 'r')
+    codes = csv.reader(f)
     cbp = CollectBrandPrices(codes)
-    cbp.collect(datetime.date(2015, 5, 18), datetime.date(2016, 5, 18))
+    
+    try:
+        cbp.collect(datetime.date(2016, 5, 17), datetime.date(2016, 5, 19))
+    except:
+        pass
